492ab883838b629797187255c2195d4371d853f7
iconoclasst <davi09bezerra70@gmail.com>
2026-05-26 09:31:44 -0300

    update

---
+++ alocacao_ponderada_memoization.py
@@ -0,0 +1,35 @@
+def computar_p(tarefas):
+    def compativel(t1, t2):
+        if t1[1] <= t2[0]:
+            return True
+        return False
+
+    p = []
+
+    for j in range(len(tarefas)):
+        ultimo = 0
+
+        for i in range(j-1, -1, -1):
+            if compativel(tarefas[i], tarefas[j]):
+                ultimo = i+1
+                break
+        p.append(ultimo)
+    return p
+
+def fb(n, tarefas):
+    p = computar_p(tarefas)
+
+    m = {}
+    m[0] = 0
+
+    def computar_opt(j):
+        if j not in m:
+            m[j] = max(computar_opt(j-1), tarefas[j-1][2] + computar_opt(p[j-1]))
+        return m[j]
+    return computar_opt(n)
+
+tarefas = [(1, 3, 4), (2, 5, 12), (2, 4, 3), (7, 12, 3), (8, 14, 15)]
+tarefas = sorted(tarefas, key=lambda x: x[1 ])
+n = len(tarefas)
+
+print(fb(n, tarefas))
